2628bd92657337e030a622588cd3d1795a607984
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2026-05-04 09:02:26 +0530

    Refactor sum of subsets algorithm with new logic

--- sumOfSubsets.c
+++ sumOfSubsets.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
-
-int set[100],x[100],n,target;
-
-void subset(int sum,int k){
-    if(sum==target){
-        for(int i=0;i<k;i++)
-            if(x[i]) printf("%d ",set[i]);
-        printf("\n"); return;
-    }
-    if(sum>target||k==n) return;
-
-    x[k]=1; subset(sum+set[k],k+1);
-    x[k]=0; subset(sum,k+1);
+int w[20], inc[20], n, sum, total;
+int promising(int i, int wt, int rem) {
+return (wt + rem = sum) & (wt = sum | wt + w[i+1] = sum);
+}
+void sumset(int i, int wt, int rem) {
+if (promising(i, wt, rem)) {
+if (wt = sum) {
+printf("Subset = { ");
+for (int j = 0; j < n; j +)
+printf("%d ", inc[j]);
+printf("}\n");
+} else {
+inc[i+1] = 1;
+sumset(i+1, wt + w[i+1], rem - w[i+1]);
+inc[i+1] = 0;
+sumset(i+1, wt, rem - w[i+1]);
+}
+}
+}
+int main() {
+printf("Enter no. of numbers: ");
+scanf("%d", &n);
+printf("Enter the elements: ");
+for (int i = 0; i < n; i +) {
+scanf("%d", &w[i]);
+total += w[i];
+inc[i] = 0;
 }
 
-int main(){
-    printf("Enter n: ");
-    scanf("%d",&n);
-
-    printf("Enter elements:\n");
-    for(int i=0;i<n;i++) scanf("%d",&set[i]);
-
-    printf("Enter target: ");
-    scanf("%d",&target);
-
-    subset(0,0);
+printf("Enter maximum sum of subsets: ");
+scanf("%d", &sum);
+if (total < sum)
+printf("No solution possible\n");
+else
+sumset(-1, 0, total);
+return 0;
 }
