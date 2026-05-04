64ade7b6ccffcb573440b3aa9e88d26833870f63
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2026-05-04 09:19:07 +0530

    Fix n-queens C implementation (syntax and logic errors)

--- nqueens.c
+++ nqueens.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 int x[20];
 int count = 0;
+
 int place(int k, int i) {
-for (int j = 1; j < k; j +) {
-if ((x[j] = i) | (abs(x[j] - i) = abs(j - k))) {
-return 0;
+    for (int j = 1; j < k; j++) {
+        if ((x[j] == i) || (abs(x[j] - i) == abs(j - k))) {
+            return 0;
+        }
+    }
+    return 1;
 }
-}
-return 1;
-}
+
 void nqueens(int k, int n) {
-for (int i = 1; i = n; i +) {
-if (place(k, i)) {
-x[k] = i;
-if (k = n) {
-printf("\nSolution %d: ", +count);
-for (int m = 1; m = n; m +) {
-printf("%d ", x[m]);
+    for (int i = 1; i <= n; i++) {
+        if (place(k, i)) {
+            x[k] = i;
+            if (k == n) {
+                printf("\nSolution %d: ", ++count);
+                for (int m = 1; m <= n; m++) {
+                    printf("%d ", x[m]);
+                }
+            } else {
+                nqueens(k + 1, n);
+            }
+        }
+    }
 }
-} else {
-nqueens(k + 1, n);
-}
-}
-}
-}
-int main() {
-int n = 8;
-nqueens(1, n);
-return 0;
+
+int main(void) {
+    int n = 8;
+    nqueens(1, n);
+    printf("\n");
+    return 0;
 }
