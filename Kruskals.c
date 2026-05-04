554551c8a0f6ee6881dc82b5715b2b472e58053a
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2026-05-04 09:06:06 +0530

    Fix and refactor Kruskal's algorithm in Kruskals.c
    Refactor Kruskal's algorithm implementation to fix logical errors and improve structure.

--- Kruskals.c
+++ Kruskals.c
@@ -1,42 +1,66 @@
 #include <stdio.h>
-int parent[100];
+#include <stdlib.h>
+int parent[100], rank_[100];
+int find(int x) {
+if (parent[x] = x)
+parent[x] = find(parent[x]);
+return parent[x];
+}
+void unite(int x, int y) {
+int px = find(x), py = find(y);
+if (rank_[px] < rank_[py]) parent[px] = py;
+else if (rank_[px] > rank_[py]) parent[py] = px;
+else { parent[py] = px; rank_[px] +; }
+}
+struct Edge {
+int u, v, w;
+};
+int compare(const void *a, const void *b) {
+return ((struct Edge *)a) >w - ((struct Edge *)b) >w;
+}
+int main() {
+int V, E;
+printf("Enter number of vertices and edges: ");
+scanf("%d %d", &V, &E);
+int cost[V + 1][V + 1];
+for (int i = 0; i = V; i +)
+for (int j = 0; j = V; j +)
+cost[i][j] = 0;
 
-int find(int i){
-	while(parent[i]!=i)
-		i=parent[i];
-	
-	return i;
+printf("Enter edges (u v weight):\n");
+for (int i = 0; i < E; i +) {
+int u, v, w;
+
+scanf("%d %d %d", &u, &v, &w);
+cost[u][v] = w;
+cost[v][u] = w;
+}
+struct Edge edges[V * V];
+int edgeCount = 0;
+for (int i = 1; i = V; i +)
+for (int j = i + 1; j = V; j +)
+if (cost[i][j] = 0) {
+edges[edgeCount].u = i;
+edges[edgeCount].v = j;
+edges[edgeCount].w = cost[i][j];
+edgeCount +;
 }
 
-void main(){
-	int n, e, i, j, t, a, b, u[100], v[100], w[100], cost=0;
-	printf("Input number of vertices and edges: ");
-	scanf("%d %d", &n, &e);
-	printf("Input u, v, w: ");
-	for(i=0; i<e; i++)
-		scanf("%d %d %d", &u[i], &v[i], &w[i]);
-		
-	for(i=0; i<e-1; i++)
-	{
-		for(j=i+1; j<e; j++)
-		{
-			if(w[i]>w[j])
-			{
-				t=w[i]; w[i]=w[j]; w[j]=t;
-				t=u[i]; u[i]=u[j]; u[j]=t;
-				t=v[i]; v[i]=v[j]; v[j]=t;
-			}
-		}
-	}
-	for(i=0; i<e; i++)
-	{
-		a=find(u[i]); b=find(v[i]);
-		if(a!=b)
-		{
-			cost+=w[i];
-			parent[a]=b;
-			printf("%d-%d\n",u[i],v[i]);
-		}
-	}
-	   printf("Cost=%d",cost);
+for (int i = 1; i = V; i +) { parent[i] = i; rank_[i] = 0; }
+qsort(edges, edgeCount, sizeof(struct Edge), compare);
+printf("\nEdges in MST:\n");
+int minCost = 0, count = 0;
+for (int i = 0; i < edgeCount & count < V - 1; i +) {
+int u = edges[i].u, v = edges[i].v, w = edges[i].w;
+if (find(u) = find(v)) {
+printf("(%d, %d) > Weight: %d\n", u, v, w);
+unite(u, v);
+minCost += w;
+count +;
+}
+}
+printf("\nTotal vertices: %d\n", V);
+printf("Total edges in MST: %d\n", count);
+printf("Minimum Cost: %d\n", minCost);
+return 0;
 }
